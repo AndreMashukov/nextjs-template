cf771b3b0c0defc32c825a6302da612412531c8e
Andrey Mashukov <andre.mashukov@gmail.com>
2020-02-17 16:09:51 +0800

    use palette color for background

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
-import './Home.scss';
 import { Card, CardContent, Typography, CardActions, Button, Theme } from '@material-ui/core';
 import { withStyles, createStyles } from '@material-ui/core/styles';
+import './Home.scss';
+
+interface Props {
+  classes: {
+    root: string;
+  };
+}
 
 const styles = ({ palette }: Theme) => createStyles({
   root: {
@@ -11,50 +16,30 @@ const styles = ({ palette }: Theme) => createStyles({
   }
 });
 
-const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center'
-  },
-  card: {
-    width: 275
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)'
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  }
-});
-
-const HomeMain = () => {
-  const classes = useStyles();
-
-  return (
-    <div className="home-layout">
-      <div className={classes.root}>
-        <Card className={classes.card}>
-          <CardContent>
-            <Typography className={classes.title} color="textSecondary" gutterBottom>
-              Categories
-            </Typography>
-            <Typography variant="h5" component="h2">
-              Consumer Electronics
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">See More</Button>
-          </CardActions>
-        </Card>
+export const HomeMain = class extends React.Component<Props> {
+  render() {
+    return (
+      <div className={this.props.classes.root}>
+        <div className="home-layout">
+          <div className="card-root">
+            <Card className="card">
+              <CardContent>
+                <Typography color="textSecondary" gutterBottom>
+                  Categories
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  Consumer Electronics
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">See More</Button>
+              </CardActions>
+            </Card>
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export const Home = withStyles(styles) (HomeMain);
