621232339df5b9fa0e98690cb377489932055cdb
Andrey Mashukov <andre.mashukov@gmail.com>
2020-02-17 16:35:15 +0800

    minor changes

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -3,21 +3,9 @@ import { AppBar, Grid, Toolbar } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import HomeIcon from '@material-ui/icons/Home';
-import { CATEGORIES } from '../../global-definitions';
 import './NavBar.scss';
 
 export const NavBar = class extends React.Component {
-  // tslint:disable-next-line: no-any
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      categories: []
-    };
-  }
-  componentDidMount = async () => {
-    const cats = CATEGORIES;
-    this.setState({ categories: cats });
-  };
   render() {
     return (
       <AppBar position="static">
