c05af2464b22f5132a9ab1a3d092b380154fd346
Andrey Mashukov <andre.mashukov@gmail.com>
2020-02-17 12:09:47 +0800

    Added a card

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
 import './Home.scss';
+import { Card, CardContent, Typography, CardActions, Button } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    display: 'flex',
+    // height: '-20vh',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  card: {
+    width: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
 
 export const Home = () => {
+  const classes = useStyles();
+
   return (
     <div className="home-layout">
-      Home
+      <div className={classes.root}>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography className={classes.title} color="textSecondary" gutterBottom>
+              Categories
+            </Typography>
+            <Typography variant="h5" component="h2">
+              Consumer Electronics
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">See More</Button>
+          </CardActions>
+        </Card>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,9 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/styles';
 import { Home } from '../components';
 
-const useStyles = makeStyles({
-  root: {
-    display: 'flex',
-    // height: '100vh',
-    width: '100vw',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  card: {
-    width: 275
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)'
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  }
-});
-
 export default () => {
-  const classes = useStyles();
   return (
-    <div className={classes.root}>
+    <div>
       <Home />
     </div>
   );
